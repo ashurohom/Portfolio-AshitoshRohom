a116ac131ba55c8fa44d1abd7343a0adc329cdfd
ashurohom <ashitosh.rohom@gmail.com>
2025-09-30 20:57:08 +0530

    add tailwind file

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import './App.css'
 
 function App() {
   return (
     <>
-      <div>Ashitosh Portfolio</div>
+          <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind is Working! 🚀
+      </h1>
+      <p className="mt-4 text-gray-600">
+        You’ve successfully installed Tailwind CSS in React Vite 🎉
+      </p>
+    </div>
     </>
   )
 }
